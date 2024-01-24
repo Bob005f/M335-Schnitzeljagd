01930eb5fc818e105c04970ac38e25e2fe3edca4
Ian Liu <ian_liu@sluz.ch>
2024-01-24 14:10:52 +0100

    bababooey

--- schnitzeljagd/src/app/tab1/tab1.page.ts
+++ schnitzeljagd/src/app/tab1/tab1.page.ts
@@ -11,4 +11,15 @@ import { ExploreContainerComponent } from '../explore-container/explore-containe
 })
 export class Tab1Page {
   constructor() {}
+  const image = await Camera.getPhoto({
+    quality: 90,
+    allowEditing: true,
+    resultType: CameraResultType.Uri
+  });
+
+  // image.webPath will contain a path that can be set as an image src.
+  // You can access the original file using image.path, which can be
+  // passed to the Filesystem API to read the raw data of the image,
+  // if desired (or pass resultType: CameraResultType.Base64 to getPhoto)
+  var imageUrl = image.webPath;
 }
