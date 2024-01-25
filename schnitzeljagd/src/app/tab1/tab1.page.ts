f5eadd35a8a850b9ea1cab8cd6827652142410c2
Ian Liu <ian_liu@sluz.ch>
2024-01-25 12:34:25 +0100

    all my fellas

--- schnitzeljagd/src/app/tab1/tab1.page.ts
+++ schnitzeljagd/src/app/tab1/tab1.page.ts
@@ -40,7 +40,7 @@ export class Tab1Page {
       resultType: CameraResultType.Uri,
     });
 
-    // image.webPath will contain a path that can be set as an image src.
+    // image.webPath will contain a path that can be set as an image src
     // You can access the original file using image.path, which can be
     // passed to the Filesystem API to read the raw data of the image,
     // if desired (or pass resultType: CameraResultType.Base64 to getPhoto)
