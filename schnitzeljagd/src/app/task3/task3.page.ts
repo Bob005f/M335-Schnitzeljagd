acde09be9e9537217cf03096d4030f2fb4276b83
Ian Liu <ian_liu@sluz.ch>
2024-01-26 09:09:34 +0100

    modified geolocation

--- schnitzeljagd/src/app/task3/task3.page.ts
+++ schnitzeljagd/src/app/task3/task3.page.ts
@@ -1,62 +1,126 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
+import { Router } from '@angular/router';
 import { Geolocation } from '@capacitor/geolocation';
+import { log } from '@angular-devkit/build-angular/src/builders/ssr-dev-server';
+import { NgZone } from '@angular/core';
 
 @Component({
-  selector: 'app-task3',
-  templateUrl: './task3.page.html',
-  styleUrls: ['./task3.page.scss'],
+  selector: 'app-task1',
+  templateUrl: './task1.page.html',
+  styleUrls: ['./task1.page.scss'],
   standalone: true,
   imports: [IonicModule, CommonModule, FormsModule],
 })
-export class Task3Page implements OnInit {
-  private targetLocation = { latitude: 47.071995, longitude: 8.348945 };
-  protected withinRange = false;
+export class Task1Page implements OnInit, OnDestroy {
+  private watchId: any; // Variable zum Speichern der Watch-ID
+  public currentLocation: { latitude: number; longitude: number } = {
+    latitude: 0,
+    longitude: 0,
+  };
+  public coordinatesMatch: boolean = false;
+  private zielDestination = {
+    latitude: 47.071995,
+  longitude: 8.348945,
+};
 
-  constructor() {}
+constructor(
+  private router: Router,
+  private ngZone: NgZone,
+) {}
 
-  ngOnInit() {}
-  async checkLocation() {
-    const position = await Geolocation.getCurrentPosition();
-    const distance = this.getDistanceFromLatLonInKm({
-      lat1: position.coords.latitude,
-      lon1: position.coords.longitude,
-      lat2: this.targetLocation.latitude,
-      lon2: this.targetLocation.longitude,
-    });
+public alertButtons = [
+  {
+    text: 'Nein',
+    role: 'cancel',
+    handler: () => {
+      console.log('Alert canceled');
+    },
+  },
+  {
+    text: 'Ja',
+    role: 'confirm',
+    handler: () => {
+      console.log('Alert confirmed');
+      this.router.navigate(['/tabs']);
+    },
+  },
+];
 
-    const radius = 0.05; // Radius in Kilometern, z.B. 50 Meter
-    this.withinRange = distance <= radius;
+async ngOnInit() {
+  this.watchId = await Geolocation.watchPosition(
+    {
+      enableHighAccuracy: true, // Erhöht die Genauigkeit der Position
+    },
+    (position) => {
+      this.updateCurrentLocation(position);
+    },
+  );
+}
+
+updateCurrentLocation(location: any) {
+  this.ngZone.run(() => {
+    this.currentLocation = {
+      latitude: location.coords.latitude,
+      longitude: location.coords.longitude,
+    };
+
+    console.log('Aktuelle Position aktualisiert:', this.currentLocation);
+
+    const schwellenwertInKilometern = 0.015;
+    const distanz = this.berechneDistanz(
+      this.currentLocation,
+      this.zielDestination,
+    );
+    this.coordinatesMatch = distanz <= schwellenwertInKilometern;
+    console.log(
+      'distanz',
+      distanz,
+      'schwellenwertInKilometern',
+      schwellenwertInKilometern,
+      'match:',
+      this.coordinatesMatch,
+    );
+  });
+}
+
+private berechneDistanz(
+  coord1: { latitude: number; longitude: number },
+coord2: { latitude: number; longitude: number },
+): number {
+  const R = 6371; // Radius der Erde in Kilometern
+  const dLat = this.gradZuRad(coord2.latitude - coord1.latitude);
+  const dLon = this.gradZuRad(coord2.longitude - coord1.longitude);
+
+  const a =
+    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    Math.cos(this.gradZuRad(coord1.latitude)) *
+    Math.cos(this.gradZuRad(coord2.latitude)) *
+    Math.sin(dLon / 2) *
+    Math.sin(dLon / 2);
+
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  const distance = R * c; // Distanz in Kilometern
+  console.log(distance);
+  return distance;
+}
+
+private gradZuRad(grad: number): number {
+  return (grad * Math.PI) / 180;
+}
+
+nextPage() {
+  if (this.watchId != null) {
+    Geolocation.clearWatch({ id: this.watchId });
   }
+  this.router.navigate(['/task2']);
+}
 
-  private getDistanceFromLatLonInKm({
-    lat1,
-    lon1,
-    lat2,
-    lon2,
-  }: {
-    lat1: any;
-    lon1: any;
-    lat2: any;
-    lon2: any;
-  }) {
-    var R = 6371; // Radius der Erde in Kilometern
-    var dLat = this.deg2rad({ deg: lat2 - lat1 });
-    var dLon = this.deg2rad({ deg: lon2 - lon1 });
-    var a =
-      Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-      Math.cos(this.deg2rad({ deg: lat1 })) *
-        Math.cos(this.deg2rad({ deg: lat2 })) *
-        Math.sin(dLon / 2) *
-        Math.sin(dLon / 2);
-    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-    var d = R * c; // Distanz in Kilometern
-    return d;
+ngOnDestroy() {
+  if (this.watchId != null) {
+    Geolocation.clearWatch({ id: this.watchId });
   }
-
-  private deg2rad({ deg }: { deg: any }) {
-    return deg * (Math.PI / 180);
-  }
+}
 }
